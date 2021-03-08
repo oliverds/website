39041151b8740db5995a8913882cc0a7c5281b4c
Oliver Jimenez-Servin <oliverds@users.noreply.github.com>
2021-03-07 18:49:31 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,13 @@ use Radiocubito\Contentful\Models\Post;
 Route::get('/', function () {
     return view('home', [
         'author' => User::first(),
-        'posts' => Post::published()->orderBy('published_at', 'desc')->get(),
+        'posts' => Post::published()->ofType('post')->orderBy('published_at', 'desc')->get(),
     ]);
 })->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::get('/now', function () {
-    return view('now', ['author' => User::first()]);
-})->name('now');
-
-Route::get('/contact', function () {
-    return view('contact', ['author' => User::first()]);
-})->name('contact');
 
 Route::get('/{post:slug}', function (Post $post) {
     abort_unless($post->isPublished(), 404);
