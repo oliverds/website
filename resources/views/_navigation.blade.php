3d1f26a16819ddf907a5a0e4c797836c5af174c1
Oliver Servín <oliverds@users.noreply.github.com>
2021-09-09 17:38:41 -0500

    Update _navigation.blade.php

--- resources/views/_navigation.blade.php
+++ resources/views/_navigation.blade.php
@@ -2,7 +2,7 @@
     <p class="text-sm lg:text-base text-gray-500 mt-4 mb-3">
         <a class="hover:underline hover:text-gray-700 font-medium transition" href="/now">Now</a>
         <span aria-hidden="true"> · </span>
-        <a class="hover:underline hover:text-gray-700 font-medium transition" href="https://twitter.com/olrjs">Twitter</a>
+        <a class="hover:underline hover:text-gray-700 font-medium transition" href="https://twitter.com/oliverds_">Twitter</a>
         <span aria-hidden="true"> · </span>
         <a class="hover:underline hover:text-gray-700 font-medium transition" href="https://github.com/oliverds/">Github</a>
         <span aria-hidden="true"> · </span>
