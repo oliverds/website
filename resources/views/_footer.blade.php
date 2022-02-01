eb5bd1e019badbf90afad4227ae79af82c146f2c
Oliver Servín <oliverds@users.noreply.github.com>
2022-02-01 12:09:16 -0600

    Update _footer.blade.php

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mt-12 text-center">
     <p class="text-sm text-gray-500 mt-4 mb-3">
-        Published with <a href="https://mitophp.com" class="hover:underline">Mito</a>
+        Published with <a href="https://usemito.com" class="hover:underline">Mito</a>
     </p>
 
     <p class="text-sm text-gray-500 mt-4 mb-3">
